5b9c1f5f831a817d6e2d7029def972e328b530eb
salmataherfouad71-dev <salma.taherfouad71@gmail.com>
2026-08-26 18:52:33 +0300

    Smart Product Search

---
+++ Assignment.12/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment._12
+{
+    public class Product
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Category { set; get; }//"Electronics","Clothing","Food","Books"
+        public double Price { set; get; }
+        public int Stock { set; get; }
+        public static List<Product> SearchProducts(List<Product>products,Func<Product,bool>Check)
+        {
+            List<Product> result=new List<Product>();
+            foreach (Product product in products)
+            {
+                if (Check(product))
+                { result.Add(product); }
+            }
+            return result;
+        }
+        public override string ToString()
+        {
+            return $"{Name}-${Price}(Stock:{Stock})";
+        }
+    }
+}
--- Assignment.12/Program.cs
+++ Assignment.12/Program.cs
@@ -1,9 +1,45 @@
-﻿namespace Assignment._12
+﻿using System.Collections;
+
+namespace Assignment._12
 {
-    internal class Program
+    public class Program
     {
+        public static void Print(List<Product> products)
+        {
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product);
+            }
+        }
         static void Main(string[] args)
         {
+            #region SmartProductSearch
+            List<Product> Catalog = new()
+            {
+                new Product{Id=1,Name="Laptop",Category="Electronics",Price=1200,Stock=10},
+                new Product{Id=2,Name="Phone",Category="Electronics",Price=800,Stock=25},
+                new Product{Id=3,Name="T-Shirt",Category="Clothing",Price=30,Stock=100},
+                new Product{Id=4,Name="Jeans",Category="Clothing",Price=60,Stock=50},
+                new Product{Id=5,Name="Chocolate",Category="Food",Price=5,Stock=200},
+                new Product{Id=6,Name="Coffee Beans",Category="Food",Price=15,Stock=80},
+                new Product{Id=7,Name="C# Book",Category="Books",Price=45,Stock=30},
+                new Product{Id=8,Name="Novel",Category="Books",Price=20,Stock=60},
+                new Product{Id=9,Name="Headphones",Category="Electronics",Price=150,Stock=40},
+                new Product{Id=10,Name="Jacket",Category="Clothing",Price=120,Stock=15},
+            };
+            Console.WriteLine("---Electronics---");
+            Print(Product.SearchProducts(Catalog, (Product p) => p.Category == "Electronics"));
+            Console.WriteLine();
+            Console.WriteLine("---Under $50---");
+            Print(Product.SearchProducts(Catalog, (Product p) => p.Price <50));
+            Console.WriteLine();
+            Console.WriteLine("---In Stock---");
+            Print(Product.SearchProducts(Catalog, (Product p) => p.Stock > 0));
+            Console.WriteLine();
+            Console.WriteLine("---Clothing Under $100---");
+            Print(Product.SearchProducts(Catalog, (Product p) => p.Category == "Clothing"&& p.Price<100));
+            Console.WriteLine();
+            #endregion
         }
     }
 }
